99677497abf3a6caefd1b24caad2286ef92acb79
Yizark30 <yizark.farrach13455@est.uca.edu.ni>
2023-05-19 20:53:06 -0600

    Delete Categoria Corregido

--- Categoria/DeleteCategoria.php
+++ Categoria/DeleteCategoria.php
@@ -1,9 +1,14 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]=="POST"){
+if($_SERVER["REQUEST_METHOD"]=="DELETE"){
     require_once '../conexion.php';
-    $idCategoria = $_POST["idCategoria"];
+    $idCategoria = $_GET["idCategoria"];
     $my_query = "DELETE from categoria where idCategoria = $idCategoria";
     $result = $mysql -> query($my_query);
+    if($result == true){
+        echo "Categoria Eliminada...";
+    }else{
+        echo "Error al eliminar Categoria...";
+    }
 
 }else{
     echo"Error desconocido";
