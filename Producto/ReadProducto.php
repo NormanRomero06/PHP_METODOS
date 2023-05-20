9b2a9311076a9ce2f6bd0c12b813f0ceb91c87a9
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-05-19 21:45:34 -0600

    Se arregló la identación [Se ve bonito]

--- Producto/ReadProducto.php
+++ Producto/ReadProducto.php
@@ -1,19 +1,19 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "GET"){
-      require_once '../conexion.php' ;
-      $my_query = "SELECT producto.idProducto, producto.nombre, producto.precio, producto.descripcion, producto.cantidad, producto.imagen, categoria.nombre AS 'Categoria Nombre'
-      FROM producto
-      INNER JOIN categoria
-      ON producto.Categoria_idCategoria = categoria.idCategoria";
-      $result = $mysql->query($my_query);
-      if ($mysql->affected_rows > 0) {
-          $json = "{\"data\":[";
+    require_once '../conexion.php';
+    $my_query = "SELECT producto.idProducto, producto.nombre, producto.precio, producto.descripcion, producto.cantidad, producto.imagen, categoria.nombre AS 'Categoria Nombre'
+    FROM producto
+    INNER JOIN categoria
+    ON producto.Categoria_idCategoria = categoria.idCategoria";
+    $result = $mysql->query($my_query);
+    if ($mysql->affected_rows > 0) {
+        $json = "{\"data\":[";
         while ($row = $result->fetch_assoc()){
             $json = $json.json_encode($row);
             $json=$json.",";
         }
-         $json=substr(trim($json),0,-1);
-         $json = $json."]}";
+        $json = substr(trim($json), 0, -1);
+        $json = $json . "]}";
         }
 echo $json;
 $result->close();
--- Usuario/ReadUsuario.php
+++ Usuario/ReadUsuario.php
@@ -1,16 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "GET"){
-      require_once '../conexion.php' ;
+    require_once '../conexion.php';
       $my_query = "SELECT * from Usuario";
-      $result = $mysql->query($my_query);
-      if ($mysql->affected_rows > 0) {
-          $json = "{\"data\":[";
+    $result = $mysql->query($my_query);
+    if ($mysql->affected_rows > 0) {
+        $json = "{\"data\":[";
         while ($row = $result->fetch_assoc()){
             $json = $json.json_encode($row);
             $json=$json.",";
         }
-         $json=substr(trim($json),0,-1);
-         $json = $json."]}";
+        $json = substr(trim($json), 0, -1);
+        $json = $json . "]}";
         }
 echo $json;
 $result->close();
