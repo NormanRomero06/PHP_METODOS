18c66ad610d656e82a6450f0bcd720f8e4bc7157
Yizark30 <yizark.farrach13455@est.uca.edu.ni>
2023-05-19 21:08:18 -0600

    IDC de producto corregido y funcionando

--- Producto/IDC_Producto.php
+++ Producto/IDC_Producto.php
@@ -1,7 +1,7 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]=="GET"){
     require_once '../conexion.php'; 
-    $idCategoria = $_GET["idProducto"];
+    $idProducto = $_GET["idProducto"];
     $my_query = "select * from producto where idProducto ='" .$idProducto."'";
 
     $result = $mysql -> query($my_query);
