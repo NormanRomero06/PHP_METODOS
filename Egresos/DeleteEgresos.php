c539db2a1139c43cf6fda26bd899b53c9ecb66e2
Andrea González <andrea.gonzalez14131@est.uca.edu.ni>
2023-05-19 18:43:09 -0600

    Eliminar egresos modificado

--- Egresos/DeleteEgresos.php
+++ Egresos/DeleteEgresos.php
@@ -1,9 +1,15 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     require_once '../conexion.php';
-    $idCategoria = $_POST["idPagos"];
+    $idPagos = $_GET["idPagos"];
     $my_query = "DELETE from egresos where idPagos = $idPagos";
     $result = $mysql -> query($my_query);
+
+    if($result == true){
+        echo "Egreso eliminado satisfactoriamente...";
+    }else{
+        echo "Error al eliminar egreso...";
+    }
 
 }else{
     echo"Error desconocido";
